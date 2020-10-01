f014ff8d9c4dda1060ab3cecd77e1b88dcece6cd
Madison Stanford <madison.stanford@gmail.com>
2020-10-01 09:57:08 -0700

    Add route tests to verify res.body for /server/ and /products/

--- test/server.test.js
+++ test/server.test.js
@@ -1,12 +1,12 @@
 const request = require("supertest");
 const app = require("../server/index.js");
 
+
 describe('dummy test to make sure jest is working', () => {
   test('checks if true is true', () => {
     expect(true).toBe(true);
   });
 });
-
 
 describe("Test the root path", () => {
   test("It should respond to GET request with status code 200", () => {
@@ -22,11 +22,11 @@ describe("Test the root path", () => {
 describe("Test the /products/ path", () => {
   test("It should respond to a GET request with status code 200", () => {
     return request(app)
-      .get(`/products/0`)
+      .get("/products/0")
       .then( (res) => {
         expect(res.statusCode).toBe(200);
-      })
-      .catch( (err) => { console.log('There was an error with test: ', err)});
+        expect(res.body.imageUrls[0]).toBe("https://hrr48madisonfecbrazil.s3-sa-east-1.amazonaws.com/0.jpg");
+      });
   })
 });
 
@@ -36,15 +36,12 @@ describe("Test the /server/ path", () => {
       .get(`/server/`)
       .then( (res) => {
         expect(res.statusCode).toBe(200);
-      })
-      .catch( (err) => { console.log('There was an error with test: ', err)});
+        expect(res.body[0].imageUrls[0]).toBe("https://hrr48madisonfecbrazil.s3-sa-east-1.amazonaws.com/0.jpg");
+
+      });
   })
 });
 
-afterAll(async () => {
-	await new Promise( (resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 500);
-  });
+afterAll((done) => {
+  done();
 });
