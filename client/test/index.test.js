ae3207c050147c55ebabfa5ff905446783433109
Madison Stanford <madison.stanford@gmail.com>
2020-09-30 21:28:29 -0700

    Configure babel, enzyme, and jest in order to test React

--- client/test/index.test.js
+++ client/test/index.test.js
@@ -1,3 +1,11 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
 describe('dummy test to make sure jest is working', () => {
   test('checks if true is true', () => {
     expect(true).toBe(true);
